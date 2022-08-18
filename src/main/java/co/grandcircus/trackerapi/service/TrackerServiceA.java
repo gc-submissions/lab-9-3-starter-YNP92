380bdb4f98b6e3b6205fd32ab0598490b8f64770
YNP92 <yaksh013@gmail.com>
2022-08-17 22:07:19 -0400

    only have top 5 left

--- src/main/java/co/grandcircus/trackerapi/service/TrackerServiceA.java
+++ src/main/java/co/grandcircus/trackerapi/service/TrackerServiceA.java
@@ -1,7 +1,7 @@
 package co.grandcircus.trackerapi.service;
 
 import java.util.ArrayList;
-import java.util.Arrays;
+
 import java.util.List;
 import java.util.Map;
 import java.util.TreeMap;
@@ -12,112 +12,118 @@ import co.grandcircus.trackerapi.model.CountPair;
 @Service
 public class TrackerServiceA implements TrackerService {
 
-	static public List<CountPair> countPairList = new ArrayList<>();
-	private List<String> tokensInOrder;
+    static public List<CountPair> countPairList = new ArrayList<>();
+    private List<String> tokensInOrder = new ArrayList<>();
 
-	@Override
-	public void add(String token) {
-		tokensInOrder.add(token);
-		boolean found = false;
-		for (CountPair countPair : countPairList) {
-			if (countPair.getToken().equals(token)) {
-				countPair.setCount(countPair.getCount() + 1);
-				found = true;
-			}
-		}
-		if (found == false) {
-			countPairList.add(new CountPair(token, 1));
-		}
-	}
+    @Override
+    public void add(String token) {
+        tokensInOrder.add(token);
+        boolean found = false;
+        for (CountPair countPair : countPairList) {
+            if (countPair.getToken().equals(token)) {
+                countPair.setCount(countPair.getCount() + 1);
+                found = true;
+            }
+        }
+        if (found == false) {
+            countPairList.add(new CountPair(token, 1));
+        }
+    }
 
-	@Override
-	public void reset() {
-		countPairList.clear();
-	}
+    @Override
+    public void reset() {
+        countPairList.clear();
+        tokensInOrder.clear();
+    }
 
-	@Override
-	public int getTotalCount() {
+    @Override
+    public int getTotalCount() {
 
-		return countPairList.size();
-	}
+        return countPairList.size();
+    }
 
-	@Override
-	public boolean getTokenExists(String token) {
-		boolean found = false;
-		for (CountPair countPair : countPairList) {
-			if (countPair.getToken().equals(token)) {
-				found = true;
-			}
-		}
-		return found;
-	}
+    @Override
+    public boolean getTokenExists(String token) {
+        boolean found = false;
+        for (CountPair countPair : countPairList) {
+            if (countPair.getToken().equals(token)) {
+                found = true;
+            }
+        }
+        return found;
+    }
 
-	@Override
-	public int getTokenCount(String token) {
-		int countPairCount = 0;
-		for (CountPair countPair : countPairList) {
-			if (countPair.getToken().equals(token)) {
-				countPairCount = countPair.getCount();
-			}
-		}
-		return countPairCount;
-	}
+    @Override
+    public int getTokenCount(String token) {
+        int countPairCount = 0;
+        for (CountPair countPair : countPairList) {
+            if (countPair.getToken().equals(token)) {
+                countPairCount = countPair.getCount();
+            }
+        }
+        return countPairCount;
+    }
 
-	@Override
-	public String getLatest() {
-		return tokensInOrder.get(tokensInOrder.size() - 1);
-	}
+    @Override
+    public String getLatest() {
+        if (tokensInOrder.size() >= 1) {
+            return tokensInOrder.get(tokensInOrder.size() - 1);
+        }
+        return "";
+    }
 
-	@Override
-	public CountPair getTop() {
-		if (countPairList.size() == 0) {
-			return new CountPair("", 0);
-		} else {
-			int highest = 0;
-			CountPair topCount = new CountPair();
-			for (CountPair countPair : countPairList) {
-				if (countPair.getCount() > highest)  {
-					topCount = countPair;
-					highest = countPair.getCount();
-				}
-			}
-			return topCount;
-		}
+    @Override
+    public CountPair getTop() {
+        if (countPairList.size() == 0) {
+            return new CountPair("", 0);
+        } else {
+            int highest = 0;
+            CountPair topCount = new CountPair();
+            for (CountPair countPair : countPairList) {
+                if (countPair.getCount() > highest) {
+                    topCount = countPair;
+                    highest = countPair.getCount();
+                }
+            }
+            return topCount;
+        }
 
-	}
+    }
 
-	@Override
-	public List<String> getLatest5() {
-		List<String> lastFiveTokens = new ArrayList<>();
+    @Override
+    public List<String> getLatest5() {
+        List<String> lastFiveTokens = new ArrayList<>();
 
-		if (tokensInOrder.size() >= 5) {
-			for (int i = (tokensInOrder.size() - 1); i < (tokensInOrder.size() - 6); i--) {
-				lastFiveTokens.add(tokensInOrder.get(i));
-			}
-			return lastFiveTokens;
-		} else {
-			return tokensInOrder;
-		}
+        if (tokensInOrder.size() >= 5) {
+            for (int i = (tokensInOrder.size() - 1); i > (tokensInOrder.size() - 5); i--) {
+                lastFiveTokens.add(tokensInOrder.get(i));
+            }
+            return lastFiveTokens;
+        } else {
+            return tokensInOrder;
+        }
 
 
-	}
+    }
 
-	@Override
-	public List<CountPair> getTop5() {
-		List<CountPair> top5List = new ArrayList<>();
-		
-		if (countPairList.size() == 0) {
-			top5List.add(new CountPair("", 0));
-			return top5List;
-		} else if (countPairList.size() <= 5) {
-			return countPairList;
-		} else {
-			Map<String,Integer> top5 = new TreeMap<>();
-			for (CountPair countPair : countPairList) {
-				
-			}
-			return top5List;
-		}
-	}
+    @Override
+    public List<CountPair> getTop5() {
+        // List<CountPair> top5List = new ArrayList<>();
+
+        // if (countPairList.size() == 0) {
+        // top5List.add(new CountPair("", 0));
+        // return top5List;
+        // } else if (countPairList.size() <= 5) {
+        // return countPairList;
+        // } else {
+        // Map<String, Integer> top5 = new TreeMap<>();
+        // for (CountPair countPair : countPairList) {
+
+        // }
+        // return top5List;
+        // }
+
+        return null;
+    }
 
 }
